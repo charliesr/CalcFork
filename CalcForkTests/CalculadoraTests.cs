c22decf3761928e5dd8d33feba0b4e62659fc58f
Pere Zamora <pere.zamora@atmira@com>
2018-03-23 07:04:29 +0100

    añadir test

---
+++ CalcForkTests/CalculadoraTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalcFork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcFork.Tests
+{
+    [TestClass()]
+    public class CalculadoraTests
+    {
+        ICalculadora iCalculdora = new Calculadora();
+
+        [TestMethod()]
+        public void SumaTest()
+        {
+            Assert.IsFalse(iCalculdora.Suma(2,7) == 10);
+        }
+    }
+}
