7b29cb8e0ef09bbeb987b8227f775101106035e0
Carles Sanchez Romero <carles.sanchez@atmira.com>
2018-03-28 17:47:54 +0200

    Crea funcionalidad Resta

--- CalcFork/Calculadora.cs
+++ CalcFork/Calculadora.cs
@@ -12,5 +12,10 @@ namespace CalcFork
         {
             return num1 + num2;
         }
+
+        public int Resta(int num1, int num2)
+        {
+            return num1-num2;
+        }
     }
 }
