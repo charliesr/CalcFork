5c5e92b3878727ad4e4c20c28ab50eb118dc582d
Daniel Madrigal <danieltristan1991@gmail.com>
2018-03-30 19:30:52 +0200

    Implementacion de Interfaz

---
+++ CalcFork/IDivision.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcFork
+{
+    public interface IDivision
+    {
+        int Division(int num1 , int num2);
+    }
+}
